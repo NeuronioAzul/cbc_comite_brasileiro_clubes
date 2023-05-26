a8ab053fd3422346ca66a430a4fd4d48e6faa48b
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-26 17:16:03 -0300

    Adicionada função consumirRecursos

--- app/Http/Controllers/Api/ClubesController.php
+++ app/Http/Controllers/Api/ClubesController.php
@@ -4,26 +4,34 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Clubes;
+use App\Models\Recursos;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClubesController extends Controller
 {
     /**
      * Exibir a listagem de Clubes.
+     *
+     * @return JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
         return response()->json(Clubes::all(), 200);
     }
 
     /**
      * Grava o clube recém-criado no banco de dados
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $dados = $request->all();
 
-        $dados['saldo_disponivel'] = str_replace(',', '.', $dados['saldo_disponivel']);
+        // Trata os valores do saldo antes de inserir na tabela
+        $dados['saldo_disponivel'] = formatarSaldoBD($dados['saldo_disponivel']);
 
         Clubes::create($dados);
 
@@ -31,26 +39,46 @@ class ClubesController extends Controller
     }
 
     /**
-     * Exibe o clube selecionado
+     * Consome recursos da tabela de recursos e do clube
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function show(string $id)
+    public function consumirRecurso(Request $request): JsonResponse
     {
-        //
-    }
+        $clubeId = $request->input('clube_id');
+        $recursoId = $request->input('recurso_id');
+        $valorConsumo = formatarSaldoBD($request->input('valor_consumo'));
 
-    /**
-     * Atualisa o clube no banco de dados.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        // Buscar o clube e recurso no banco de dados
+        $clube = Clubes::find($clubeId);
+        $recurso = Recursos::find($recursoId);
 
-    /**
-     * Remove o clube do banco de dados
-     */
-    public function destroy(string $id)
-    {
-        //
+        // Verificar se o saldo disponível do clube é suficiente para o consumo
+        if ($clube->saldo_disponivel < $valorConsumo) {
+            return response()->json('O saldo disponível do clube é insuficiente', 400);
+        }
+
+        // Atualizar os saldos
+        $saldoAnterior = $clube->saldo_disponivel;
+        $saldoAtual = $saldoAnterior - $valorConsumo;
+
+        // Atualizar o saldo do clube
+        $clube->saldo_disponivel = $saldoAtual;
+        $clube->save();
+
+        // Atualizar o saldo do recurso
+        $recurso->saldo_disponivel -= $valorConsumo;
+        $recurso->save();
+
+        // Retornar a resposta com os saldos atualizados
+        return response()->json(
+            [
+                'clube' => $clube->clube,
+                'saldo_anterior' => formatarSaldoAPI($saldoAnterior),
+                'saldo_atual' => formatarSaldoAPI($saldoAtual)
+            ],
+            200
+        );
     }
 }
