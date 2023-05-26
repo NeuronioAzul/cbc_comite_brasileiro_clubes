9e43b845e860b0aa11ba0e13049b17112a9ac219
Mauro Rocha <mauro.rocha.t@gmail.com>
2023-05-26 01:19:12 -0300

    Adicionada a Migration para a tabela clubes

---
+++ database/migrations/2023_05_26_020743_create_table_clubes.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clubes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('clube', 250);
+            $table->decimal('saldo_disponivel', 20, 2)->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clubes');
+    }
+};
