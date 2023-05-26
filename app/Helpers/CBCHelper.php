0fcf91580e65a13e1b989fc506407a8a54bef991
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-26 17:18:30 -0300

    Helper melhoria no método de tratamento de valores

--- app/Helpers/CBCHelper.php
+++ app/Helpers/CBCHelper.php
@@ -7,7 +7,7 @@ if (! function_exists('formatarSaldoAPI')) {
      * @param float $numero Valor tipo float 123.11
      * @return string Número no formato 1010,00
      */
-    function formatarSaldoAPI(float $numero = 0)
+    function formatarSaldoAPI(float $numero = 0): string
     {
         return number_format($numero, 2, ',', '');
     }
@@ -21,10 +21,10 @@ if (! function_exists('formatarSaldoBD')) {
      * @param string $numero Nos formatos 1,001.01 ou 1.001,01
      * @return string O número no formato 1010.10
      */
-    function formatarSaldoBD(string $numero = '0')
+    function formatarSaldoBD(string $numero = '0'): string
     {
-        // Remover símbolos de formatação
-        $numero = str_replace([',','.'], '', $numero);
+        // Trocar virgula por ponto
+        $numero = str_replace(',', '.', $numero);
 
         // Converter o valor para float
         $numero = floatval($numero);
