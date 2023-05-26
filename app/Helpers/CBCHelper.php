ba2fbf48002d00a33c53c2cc4f8cc28c4257bbec
Mauro Rocha <maurort@apoioprodesp.sp.gov.br>
2023-05-26 15:16:07 -0300

    Melhoria Helper Formatar valor do saldo_disponivel

--- app/Helpers/CBCHelper.php
+++ app/Helpers/CBCHelper.php
@@ -1,16 +1,37 @@
 <?php
 
 if (! function_exists('formatarSaldoAPI')) {
-    function formatarSaldoAPI($numero, $casasDecimais = 2, $separadorDecimal = ',', $separadorMilhar = '')
+    /**
+     * Formatar saldo para o formato padrão de exibição
+     *
+     * @param float $numero Valor tipo float 123.11
+     * @return string Número no formato 1010,00
+     */
+    function formatarSaldoAPI(float $numero = 0)
     {
-        return number_format($numero, $casasDecimais, $separadorDecimal, $separadorMilhar);
+        return number_format($numero, 2, ',', '');
     }
 }
 
 if (! function_exists('formatarSaldoBD')) {
 
-    function formatarSaldoBD($numero, $casasDecimais = 2, $separadorDecimal = '.', $separadorMilhar = '')
+    /**
+     * Formata o saldo para inserir no banco de dados
+     *
+     * @param string $numero Nos formatos 1,001.01 ou 1.001,01
+     * @return string O número no formato 1010.10
+     */
+    function formatarSaldoBD(string $numero = '0')
     {
-        return number_format($numero, $casasDecimais, $separadorDecimal, $separadorMilhar);
+        // Remover símbolos de formatação
+        $numero = str_replace([',','.'], '', $numero);
+
+        // Converter o valor para float
+        $numero = floatval($numero);
+
+        // Formata o valor
+        $numero = number_format($numero, 2, '.', '');
+
+        return $numero;
     }
 }
